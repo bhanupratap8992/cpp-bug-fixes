c67c28c89a68be87f35e571c5275cf04428dbb0b
bhanupratap8992 <139051597+bhanupratap8992@users.noreply.github.com>
2026-03-26 21:41:36 +0530

    Create segmentation-fault-fix.cpp

---
+++ segmentation-fault-fix.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+
+    int *arr = new int[n];
+
+    for(int i=0;i<n;i++)
+        arr[i] = i*i;
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+
+    delete[] arr;
+}
